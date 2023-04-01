248bf3977a68442295b127a03e80af13ceecae82
Chris <sirchrissarnacki@gmail.com>
2023-04-01 15:18:46 -0700

    Added SVG class

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-inquirer 
-    .prompt([
+class Svg{
+    constructor(){
+        this.textElement = ''
+        this.shapeElement = ''
+    }
+    render(){
+        return `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="300" height="200">${this.shapeElement}${this.textElement}</svg>`
+    }
+    setTextElement(text,color){
+        this.textElement = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${color}">${text}</text>`
+    }
+    setShapeElement(shape){
+        this.shapeElement = shape.render()
+    }
+}
+
+const questions = [ 
     {
         type: 'list',
         name: 'shape',
@@ -24,19 +39,31 @@ inquirer
         name: 'shapeColor',
         message: 'What color would you like the shape to be?',
     },
-])
+];
 
-const writeFile = fileContent => {
-    return new Promise((resolve, reject) => {
-        fs.writeFile('logo.txt', fileContent, err => {
-            if(err) {
-                reject(err);
-                return;
-            }
-            resolve({
-                ok: true,
-                message: 'Logo created!'
-            });
-        });
+const writeToFile = data => {
+    fs.writeFile('./lib/logo.svg', data, err => {
+        if (err) {
+         console.log(err);
+        }
+         console.log('Logo created!');
     });
-};
+}
+
+const init = () => {
+    return inquirer.prompt(questions)
+    .then(logoData => {
+        return logoData;
+    })
+}
+
+init()
+.then(logoPage => {
+    return writeToFile(logoPage);
+})
+.then(fileResponse => {
+    console.log(fileResponse.message);
+})
+.catch(err => {
+    console.log(err);
+})
